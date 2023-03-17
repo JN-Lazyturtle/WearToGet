162f3099a1514800bbf41686a512e952b8b01c8f
roux <yvan.roux@inrae.fr>
2023-03-17 16:18:58 +0100

    pdf mieux

--- external/Framework/Services/WearToGetPDFService.php
+++ external/Framework/Services/WearToGetPDFService.php
@@ -20,14 +20,15 @@ class WearToGetPDFService
          $margin = 5;
          $pdf->Image($photo, $margin, $pdf->GetY(), $width);
 
-
          $pdf->SetFont('Arial', '', 12);
          foreach ($publication->getItems() as $item){
              $pdf->SetX($width+$margin*2);
              $txt = $item->getCategory()." ".$item->getBrand();
              $pdf->MultiCell($pdf->GetPageWidth()/2, 7, $txt);
-             $pdf->Cell($pdf->GetPageWidth()/2, 7, "disponible ici", null, null, null, null, $item->getLink());
              $pdf->Ln(3);
+             $pdf->SetX($width+$margin*2+10);
+             $pdf->Cell($pdf->GetPageWidth()/2, null, "disponible ici", null, null, null, null, $item->getLink());
+             $pdf->Ln(8);
          }
 
          $pdf->generateAndDisplayPDF();
